9f14da3db61792323ae5f47fd89699dd15c218b3
koboko34 <66181314+koboko34@users.noreply.github.com>
2026-04-13 22:59:11 +0100

    Loading Vulkan library

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,92 @@
+#include <cstdlib>
+#include <format>
 #include <iostream>
+#include <stdexcept>
 
+#include "SDL3/SDL_error.h"
+#include "SDL3/SDL_log.h"
+#include "SDL3/SDL_messagebox.h"
+#include "SDL3/SDL_video.h"
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
 
-int main() {
+class SDLException : public std::runtime_error
+{
+public:
+    SDLException(const std::string &message)
+        : std::runtime_error(std::format("{} {}", message, SDL_GetError()))
+    {
+    }
+};
 
-    SDL_Window *window;
-    bool done = false;
+class App
+{
+public:
+    App() {}
+    ~App() {}
 
-    SDL_Init(SDL_INIT_VIDEO);
+    void Init()
+    {
+        if (!SDL_Init(SDL_INIT_VIDEO))
+            throw SDLException("Failed to initialise SDL!");
 
-    window = SDL_CreateWindow(
-        "Vulkan App",
-        640,
-        480,
-        SDL_WINDOW_VULKAN
-    );
+		if (!SDL_Vulkan_LoadLibrary(nullptr))
+			throw SDLException("Failed to load Vulkan library!");
 
-    if (window == NULL)
-	{
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
-        return 1;
+        // hidden to hide the window while initialisation is taking place
+        SDL_WindowFlags flags =
+            SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN;
+        pWindow = SDL_CreateWindow("Vulkan App", 640, 480, flags);
+        if (pWindow == nullptr)
+            throw SDLException("Failed to create window!");
     }
 
-	int frame = 0;
-    while (!done)
-	{
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-		{
-            if (event.type == SDL_EVENT_QUIT)
-                done = true;
+    void Shutdown()
+    {
+        SDL_DestroyWindow(pWindow);
+        SDL_Quit();
+    }
+
+    void Run()
+    {
+        SDL_ShowWindow(pWindow);
+
+        int frame = 0;
+        bool bDone = false;
+        while (!bDone)
+        {
+            SDL_Event event;
+            while (SDL_PollEvent(&event))
+            {
+                if (event.type == SDL_EVENT_QUIT)
+                    bDone = true;
+            }
+
+            std::cout << "Frame " << ++frame << "\n";
         }
 
-		std::cout << "Frame " << ++frame << "\n";
+        Shutdown();
     }
 
-    SDL_DestroyWindow(window);
+private:
+    SDL_Window *pWindow = nullptr;
+};
 
-    SDL_Quit();
-    return 0;
+int main()
+{
+    try
+    {
+        App app;
+        app.Init();
+        app.Run();
+    }
+    catch (const SDLException &e)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "%s", e.what());
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", e.what(),
+                                 nullptr);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }

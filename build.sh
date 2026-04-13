a695413aaa37fca010017a81fca3d8a9cd5c600b
koboko34 <66181314+koboko34@users.noreply.github.com>
2026-04-13 18:29:01 +0100

    Added SDL3

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-cmake -B build/debug -G Ninja -DCMAKE_BUILD_TYPE=Debug && \
+cmake -B build/debug -G Ninja \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake \
+	-DVCPKG_TARGET_TRIPLET=x64-linux && \
 cmake --build build/debug
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,42 @@
-int main()
-{
-	return 0;
+#include <iostream>
+
+#include <SDL3/SDL.h>
+
+int main() {
+
+    SDL_Window *window;
+    bool done = false;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow(
+        "Vulkan App",
+        640,
+        480,
+        SDL_WINDOW_VULKAN
+    );
+
+    if (window == NULL)
+	{
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+	int frame = 0;
+    while (!done)
+	{
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+		{
+            if (event.type == SDL_EVENT_QUIT)
+                done = true;
+        }
+
+		std::cout << "Frame " << ++frame << "\n";
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    return 0;
 }
